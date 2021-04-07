9ffe0e51f1cfeb85814de9fa1cd31ef9ac0fad09
Ryan Vandersmith <ryanvandersmith@gmail.com>
2021-04-07 17:01:23 -0600

    Updates

---
+++ precache-manifest.1da854df1d0f6fc0b2217f0d22d9e6a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf2550c481284b28e5b23c9c1f1d73f",
+    "url": "https://rvanasa.github.io/elemental-x/index.html"
+  },
+  {
+    "revision": "77e1697a78d3d1376243",
+    "url": "https://rvanasa.github.io/elemental-x/static/css/main.39ed13e4.chunk.css"
+  },
+  {
+    "revision": "f85b82fd2d3f3e46da47",
+    "url": "https://rvanasa.github.io/elemental-x/static/js/2.eab7f9f1.chunk.js"
+  },
+  {
+    "revision": "930ea72c1567d102c6a6ce440bc27014",
+    "url": "https://rvanasa.github.io/elemental-x/static/js/2.eab7f9f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77e1697a78d3d1376243",
+    "url": "https://rvanasa.github.io/elemental-x/static/js/main.9b82567f.chunk.js"
+  },
+  {
+    "revision": "536cad3ca0ff293a37d4",
+    "url": "https://rvanasa.github.io/elemental-x/static/js/runtime-main.0ab33f13.js"
+  }
+]);
